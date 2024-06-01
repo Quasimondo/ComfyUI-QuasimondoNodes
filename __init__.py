19ee49f69ef5350681fcfcd1314db871a647a9f6
Mario Klingemann <Quasimondo@users.noreply.github.com>
2024-06-01 21:03:27 +0200

    Update __init__.py
    Added RandomImageGenerator, PerlinNoiseGenerator, ImageNoiseGenerator

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from .gl_nodes import CustomShader, SpringMesh
 from .queue_nodes import VideoQueueManager, FolderQueueManager
+from .random_image_generator import RandomImageGenerator, PerlinNoiseGenerator, ImageNoiseGenerator
 
 NODE_CLASS_MAPPINGS = {
      "Custom Shader": CustomShader,
      "Spring Mesh": SpringMesh,
      "Video Queue Manager": VideoQueueManager,
      "Folder Queue Manager": FolderQueueManager,
+     "Random Image Generator": RandomImageGenerator,
+     "Perlin Noise Generator": PerlinNoiseGenerator,   
+     "Image Noise Generator": ImageNoiseGenerator,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
