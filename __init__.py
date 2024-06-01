3d5137670a5d6bb89f2cfb50f4b536a164ba3f10
Mario Klingemann <Quasimondo@users.noreply.github.com>
2024-06-01 22:50:35 +0200

    Update __init__.py
    added color match

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .gl_nodes import CustomShader, SpringMesh
 from .queue_nodes import VideoQueueManager, FolderQueueManager
 from .random_image_generator import RandomImageGenerator, PerlinNoiseGenerator, ImageNoiseGenerator
-from .extra_nodes import ImageBlendMaskBatch
+from .extra_nodes import ImageBlendMaskBatch, ColorMatch
 
 NODE_CLASS_MAPPINGS = {
      "Custom Shader": CustomShader,
@@ -12,6 +12,7 @@ NODE_CLASS_MAPPINGS = {
      "Perlin Noise Generator": PerlinNoiseGenerator,   
      "Image Noise Generator": ImageNoiseGenerator,
      "Image Blend by Mask (Batch)": ImageBlendMaskBatch,
+     "Color Match": ColorMatch,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
