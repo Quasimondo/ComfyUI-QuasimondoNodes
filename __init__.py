6c19a3f1a5f0c04f15a87aeecb6cbe8e2586c287
Mario Klingemann <Quasimondo@users.noreply.github.com>
2024-06-01 21:08:22 +0200

    Update __init__.py
    Added ImageBlendMaskBatch

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .gl_nodes import CustomShader, SpringMesh
 from .queue_nodes import VideoQueueManager, FolderQueueManager
 from .random_image_generator import RandomImageGenerator, PerlinNoiseGenerator, ImageNoiseGenerator
+from .extra_nodes import ImageBlendMaskBatch
 
 NODE_CLASS_MAPPINGS = {
      "Custom Shader": CustomShader,
@@ -10,6 +11,7 @@ NODE_CLASS_MAPPINGS = {
      "Random Image Generator": RandomImageGenerator,
      "Perlin Noise Generator": PerlinNoiseGenerator,   
      "Image Noise Generator": ImageNoiseGenerator,
+     "Image Blend by Mask (Batch)": ImageBlendMaskBatch,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
