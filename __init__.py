0c1eb69634c2d5b4cacfe50e0e07483febbda046
Mario Klingemann <Quasimondo@users.noreply.github.com>
2024-06-01 11:04:31 +0200

    Update __init__.py
    Added VideoQueueManager and FolderQueueManager

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 from .gl_nodes import CustomShader, SpringMesh
+from .queue_nodes import VideoQueueManager, FolderQueueManager
 
 NODE_CLASS_MAPPINGS = {
      "Custom Shader": CustomShader,
-     "Spring Mesh": SpringMesh
+     "Spring Mesh": SpringMesh,
+     "Video Queue Manager": VideoQueueManager,
+     "Folder Queue Manager": FolderQueueManager,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
